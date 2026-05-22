78646fb586b00f661b938d6baf34faaf3440ab46
Fathan <fathan@example.com>
2026-05-22 23:59:51 +0700

    last

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "./globals.css";
 import { AuthProvider } from "@/context/AuthContext";
-import Navbar from "@/components/Navbar";
+import Navbar from "@/components/navbar";
 
 
 export const metadata: Metadata = {
